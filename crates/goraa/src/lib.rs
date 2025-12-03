1374b794e90f127caf6aaef2b9b31870ea15c7fe
Graham Kelly <gkgoat6700@gmail.com>
2025-12-02 21:59:09 -0500

    mash

--- crates/goraa/src/lib.rs
+++ crates/goraa/src/lib.rs
@@ -1,5 +1,29 @@
 #![no_std]
 
+use core::fmt::Display;
 
-pub struct Goraa<T>(pub T);
+pub struct Goraa<T,R>(pub T, pub R);
 pub mod x64;
+#[derive(Clone, Copy,PartialEq, Eq, PartialOrd, Ord,Hash,Debug)]
+pub struct ShimLabel<L> {
+    pub wrapped: L,
+    pub kind: ShimKind,
+}
+#[derive(Clone, Copy,PartialEq, Eq, PartialOrd, Ord,Hash,Debug)]
+pub enum ShimKind {
+    None,
+    JmpD,
+}
+impl Display for ShimKind{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self{
+            ShimKind::None => Ok(()),
+            ShimKind::JmpD => write!(f,"_jd_"),
+        }
+    }
+}
+impl<L: Display> Display for ShimLabel<L>{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f,"{}{}",&self.kind,&self.wrapped)
+    }
+}
--- crates/goraa/src/x64.rs
+++ crates/goraa/src/x64.rs
@@ -1,15 +1,77 @@
 use crate::*;
 
-use portal_solutions_asm_x86_64::out::{Writer, WriterCore};
-pub trait GoraaDeps: WriterCore{
-
+use portal_pc_asm_common::types::reg::Reg;
+use portal_solutions_asm_x86_64::{
+    ConditionCode, X64Arch,
+    out::{Writer, WriterCore, arg::MemArgKind},
+};
+use rand::Rng;
+pub trait GoraaDeps: WriterCore {
+    fn mash(&mut self, cfg: X64Arch) -> Result<(), Self::Error> {
+        let sp = MemArgKind::Mem {
+            base: Reg(4),
+            offset: None,
+            disp: 0,
+            size: portal_pc_asm_common::types::mem::MemorySize::_64,
+            reg_class: portal_solutions_asm_x86_64::RegisterClass::Gpr,
+        };
+        self.xchg(cfg, &sp, &Reg(0))?;
+        self.pushf(cfg)?;
+        self.push(cfg, &0u64)?;
+        Ok(())
+    }
 }
-impl<T: WriterCore+?Sized> GoraaDeps for T{}
-impl<T: GoraaDeps + Writer<L>,L> Writer<L> for Goraa<T>{
-    fn set_label(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch, s: L) -> Result<(), Self::Error> {
-
-        self.0.set_label(cfg, s)?;
-
+impl<T: WriterCore + ?Sized> GoraaDeps for T {}
+impl<T: GoraaDeps + Writer<ShimLabel<L>>, L: Clone, R: Rng> Writer<L> for Goraa<T, R> {
+    fn set_label(
+        &mut self,
+        cfg: portal_solutions_asm_x86_64::X64Arch,
+        s: L,
+    ) -> Result<(), Self::Error> {
+        let a = loop {
+            let a = self.1.random();
+            if a % 16 != 4 {
+                break a;
+            }
+        };
+        let sp = MemArgKind::Mem {
+            base: Reg(4),
+            offset: None,
+            disp: 0,
+            size: portal_pc_asm_common::types::mem::MemorySize::_64,
+            reg_class: portal_solutions_asm_x86_64::RegisterClass::Gpr,
+        };
+        self.0.mash(cfg)?;
+        self.0.set_label(
+            cfg,
+            ShimLabel {
+                wrapped: s.clone(),
+                kind: ShimKind::None,
+            },
+        )?;
+        self.0.push(cfg, &Reg(a))?;
+        self.0.cmp0(cfg, &sp)?;
+        self.0.lea_label(
+            cfg,
+            &Reg(a),
+            ShimLabel {
+                wrapped: s.clone(),
+                kind: ShimKind::JmpD,
+            },
+        )?;
+        self.0.jcc(cfg, ConditionCode::NE, &Reg(a))?;
+        self.0.pop(cfg, &Reg(a))?;
+        self.0.popf(cfg)?;
+        self.0.xchg(cfg, &sp, &Reg(0))?;
+        self.0.push(cfg, &Reg(a))?;
+        self.0.set_label(
+            cfg,
+            ShimLabel {
+                wrapped: s.clone(),
+                kind: ShimKind::JmpD,
+            },
+        )?;
+        self.0.pop(cfg, &Reg(a))?;
         Ok(())
     }
 
@@ -19,12 +81,19 @@ impl<T: GoraaDeps + Writer<L>,L> Writer<L> for Goraa<T>{
         dest: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_),
         label: L,
     ) -> Result<(), Self::Error> {
-        self.0.lea_label(cfg, dest, label)?;
+        self.0.lea_label(
+            cfg,
+            dest,
+            ShimLabel {
+                wrapped: label,
+                kind: ShimKind::None,
+            },
+        )?;
 
         Ok(())
     }
 }
-impl<T: GoraaDeps> WriterCore for Goraa<T>{
+impl<T: GoraaDeps, R: Rng> WriterCore for Goraa<T, R> {
     type Error = T::Error;
 
     fn hlt(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch) -> Result<(), Self::Error> {
@@ -76,23 +145,43 @@ impl<T: GoraaDeps> WriterCore for Goraa<T>{
         todo!()
     }
 
-    fn push(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch, op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_)) -> Result<(), Self::Error> {
+    fn push(
+        &mut self,
+        cfg: portal_solutions_asm_x86_64::X64Arch,
+        op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_),
+    ) -> Result<(), Self::Error> {
         todo!()
     }
 
-    fn pop(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch, op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_)) -> Result<(), Self::Error> {
+    fn pop(
+        &mut self,
+        cfg: portal_solutions_asm_x86_64::X64Arch,
+        op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_),
+    ) -> Result<(), Self::Error> {
         todo!()
     }
 
-    fn call(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch, op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_)) -> Result<(), Self::Error> {
+    fn call(
+        &mut self,
+        cfg: portal_solutions_asm_x86_64::X64Arch,
+        op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_),
+    ) -> Result<(), Self::Error> {
         todo!()
     }
 
-    fn jmp(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch, op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_)) -> Result<(), Self::Error> {
+    fn jmp(
+        &mut self,
+        cfg: portal_solutions_asm_x86_64::X64Arch,
+        op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_),
+    ) -> Result<(), Self::Error> {
         todo!()
     }
 
-    fn cmp0(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch, op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_)) -> Result<(), Self::Error> {
+    fn cmp0(
+        &mut self,
+        cfg: portal_solutions_asm_x86_64::X64Arch,
+        op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_),
+    ) -> Result<(), Self::Error> {
         todo!()
     }
 
@@ -115,11 +204,19 @@ impl<T: GoraaDeps> WriterCore for Goraa<T>{
         todo!()
     }
 
-    fn u32(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch, op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_)) -> Result<(), Self::Error> {
+    fn u32(
+        &mut self,
+        cfg: portal_solutions_asm_x86_64::X64Arch,
+        op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_),
+    ) -> Result<(), Self::Error> {
         todo!()
     }
 
-    fn not(&mut self, cfg: portal_solutions_asm_x86_64::X64Arch, op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_)) -> Result<(), Self::Error> {
+    fn not(
+        &mut self,
+        cfg: portal_solutions_asm_x86_64::X64Arch,
+        op: &(dyn portal_solutions_asm_x86_64::out::arg::MemArg + '_),
+    ) -> Result<(), Self::Error> {
         todo!()
     }
 
